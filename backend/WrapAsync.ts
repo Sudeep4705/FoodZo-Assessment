cccbc7e0d98eb2b40f06c7f49fcafe4f95c4aac9
Sudeep <sudeepsudee828@gmail.com>
2025-12-22 12:23:02 +0530

    wrapAsync

--- backend/WrapAsync.ts
+++ backend/WrapAsync.ts
@@ -1,9 +1,11 @@
-const WrapAsync = function(fn){
-    return function(req,res,next){
-        fn(req,res,next).catch((err)=>next(err))
-    }
-}
+import { Request, Response, NextFunction } from "express";
 
+const WrapAsync = function (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    fn(req, res, next).catch((err: any) => next(err));
+  };
+};
 
-
-export default WrapAsync
+export default WrapAsync;
